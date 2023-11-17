3dd7c36ddae41626c041b64f52d4aa0ff10ac666
Asaka <95882188+AsakaJX@users.noreply.github.com>
2023-11-17 10:59:18 +0300

    Update sdek_parse.py

--- sdek_parse.py
+++ sdek_parse.py
@@ -9,6 +9,9 @@ def parse(orderID, element):
 
     # parsing
     search_result = soup.find_all("div", class_="tracking-main-param wide smallbm")
+    if (element >= len(search_result)):
+        print("There's no element with that element_id!")
+        sys.exit(1)
     # applying regex search on our choosen element
     regex = "<\\/strong>(.*)<\\/div>"
     if ("<span class=\"col-xs-6 nopadding\">" in str(search_result[element])):
@@ -22,23 +25,6 @@ def parse(orderID, element):
     return search_result_normalized
 
 if __name__ == "__main__":
-    # This script is parsing maintransport.ru site
-    # It accepts two arguments: package_id, element_id(to parse)
-    # Information about id of element_id could be found below.
-    
-    # HOW TO RUN:
-    # python sdek_parse.py package_id element_id
-
-    # Ex: python sdek_parse.py 1238742112 1
-
-    # Information about element_id's:
-    # 0 - package id
-    # 1 - status
-    # 2 - shipped from (city)
-    # 3 - shipped to (city)
-    # 4 - package sender
-    # There could be more than 5 information about package, BUT this's not guaranteed
-    
     if (len(sys.argv) < 3):
         sys.exit("Not enough arguments!")
 
